fe034ac7c5cbadff5dbe24bdd8f090747d1c1ccc
Thofiq Mohammed <shaikh.thofiq@gmail.com>
2024-10-08 22:28:37 +0530

    EF Code First App

---
+++ WApiDemo.Comp/Migrations/20241008164945_seedingcountryandhotel.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WApiDemo.Comp.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedingcountryandhotel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[,]
+                {
+                    { 1, "India", "IN" },
+                    { 2, "Saudi Arabia", "KSA" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[] { 1, "YSR", 1, "Ziara", 3.8999999999999999 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
---
+++ WApiDemo.Comp/WApiDemoDbContext/WApiDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WApiDemo.Comp.Models.EntityModels;
+
+namespace WApiDemo.Comp.WApiDemoDbContext
+{
+    public class WApiDbContext : DbContext
+    {
+        public WApiDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+                new Country() { Name="India", ShortName="IN", Id = 1},
+                new Country() { Name="Saudi Arabia", ShortName="KSA", Id=2}
+                );
+            modelBuilder.Entity<Hotel>().HasData(new Hotel() { Id=1, Name="Ziara", Address="YSR", Rating=3.9, CountryId=1});
+        }
+    }
+}
